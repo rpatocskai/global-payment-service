c16bf97e2cb7a3ced195a436e3a5c0cf2c413e65
rpatocskai <rpatocskai.developer@gmail.com>
2026-09-22 15:13:11 +0200

    #2 (config): Spring boot init

---
+++ backend/src/test/java/global_payment_service/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package global_payment_service.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
