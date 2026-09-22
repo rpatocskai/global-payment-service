379f0841bad7e2e28599d293e50892c85efa74af
rpatocskai <rpatocskai.developer@gmail.com>
2026-09-22 15:13:11 +0200

    #2 (config): Spring boot init

---
+++ backend/src/main/java/global_payment_service/backend/BackendApplication.java
@@ -0,0 +1,13 @@
+package global_payment_service.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
