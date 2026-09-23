34332a33a3ca84406b9d872b6b97767cb0afc2a9
rpatocskai <rpatocskai.developer@gmail.com>
2026-09-23 19:33:03 +0200

    #20, #21 (feat): external api simulation, fault currents

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -9,3 +9,15 @@ VALUES (2500.5000, 'EUR', 0)
 INSERT INTO accounts (balance, currency, version)
 VALUES (500.0000, 'USD', 0)
     ON CONFLICT DO NOTHING;
+
+INSERT INTO accounts (balance, currency, version)
+SELECT 1000.0000, 'HUF', 0
+    WHERE NOT EXISTS (SELECT 1 FROM accounts WHERE id = 1);
+
+INSERT INTO accounts (balance, currency, version)
+SELECT 2500.5000, 'EUR', 0
+    WHERE NOT EXISTS (SELECT 1 FROM accounts WHERE id = 2);
+
+INSERT INTO accounts (balance, currency, version)
+SELECT 500.0000, 'USD', 0
+    WHERE NOT EXISTS (SELECT 1 FROM accounts WHERE id = 3);
