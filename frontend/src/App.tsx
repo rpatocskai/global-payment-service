32d9ee98086c4c469833c9ed803969cf6243dad4
rpatocskai <rpatocskai.developer@gmail.com>
2026-09-23 17:18:22 +0200

    #12 (feat): account page frame impl

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,13 @@
-import {
-  AppBar,
-  Toolbar,
-  Typography,
-  Container,
-  Box,
-  Paper,
-} from "@mui/material";
+import { AppBar, Toolbar, Typography, Container, Box } from "@mui/material";
 import AccountBalanceWalletIcon from "@mui/icons-material/AccountBalanceWallet";
+import { AccountsPage } from "./pages/AccountsPage";
 
 export const App = () => {
   return (
     <Box
       sx={{ flexGrow: 1, minHeight: "100vh", bgcolor: "background.default" }}
     >
-      {/* Alap Navigációs Sáv */}
-      <AppBar position="static">
+      <AppBar position="static" sx={{ mb: 4 }}>
         <Toolbar>
           <AccountBalanceWalletIcon sx={{ mr: 2 }} />
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
@@ -23,17 +16,8 @@ export const App = () => {
         </Toolbar>
       </AppBar>
 
-      {/* Központi tartalomhely */}
-      <Container maxWidth="md" sx={{ mt: 4 }}>
-        <Paper elevation={2} sx={{ p: 4, textAlign: "center" }}>
-          <Typography variant="h5" color="primary" gutterBottom>
-            Epic 1 sikeresen felállítva! 🎉
-          </Typography>
-          <Typography variant="body1" color="text.secondary">
-            A React + TypeScript + Material UI környezet kész a fejlesztésre. Az
-            Axios kliens be van állítva a http://localhost:8080/api címre.
-          </Typography>
-        </Paper>
+      <Container maxWidth="lg">
+        <AccountsPage />
       </Container>
     </Box>
   );
